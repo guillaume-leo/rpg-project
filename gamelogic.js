673524c5030ffafb4c2f0c10f3c94d702bcf0d16
guillaume-leo <quet.guillaume.leo@gmail.com>
2021-02-24 12:31:39 +0100

    some changes

--- gamelogic.js
+++ gamelogic.js
@@ -1,6 +1,7 @@
 // SELECTION DES JOUEURS ET LANCEMENT DE LA PARTIE
 //---------------------------------------------------
 
+
 let player = 1;
 function validate() {
 
@@ -19,6 +20,11 @@ function validate() {
         break;
       }
     }
+
+
+    
+
+
 
 
 
@@ -56,3 +62,49 @@ function validate() {
 
 }
 
+
+// ------------------------------------------ GRAPH
+
+var firstCanvas = document.createElement("canvas");
+var parent = document.getElementById("graphic");
+parent.appendChild(firstCanvas);
+    
+var myChart = new Chart(firstCanvas, {
+    type: 'radar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+//---------------------------FIN DU GRAPHE
